d24c40a1f98fc5c1eb7a002a90c47fbe1515dabd
pavlorozhok <pavlo.rozhok@namecheap.com>
2025-12-03 13:10:08 +0200

    remove error

--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,13 +1,9 @@
 import type { CreateUserRequestDto } from './dto/request/create-user.request.dto';
 import type { UserResponseDto } from './dto/response/user.response.dto';
 import { usersRepository } from '../../repositories/users.repository';
-import { ApiError } from '../../common/models/errors/api-error';
 
 export class UsersService {
   public getAll = async (): Promise<UserResponseDto[]> => {
-    // await new Promise((res) => setTimeout(res, 1000));
-    throw new ApiError({ message: 'Custom Error Message', statusCode: 401 });
-    // throw new Error('qwd');
     return usersRepository.findAll();
   };
 
