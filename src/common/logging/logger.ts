cf75de2e711d8374688fd838129fbf27970b8cdb
pavlorozhok <pavlo.rozhok@namecheap.com>
2025-12-03 15:38:00 +0200

    fix logger

--- src/common/logging/logger.ts
+++ src/common/logging/logger.ts
@@ -2,15 +2,15 @@ import { pino } from 'pino';
 
 import { config } from '../config';
 
-const transport = config.env.isTest
+const transport = config.env.isDevelopment
   ? {
-      target: 'pino-pretty',
-      options: {
-        colorize: true,
-        translateTime: 'SYS:HH:MM:ss Z',
-        ignore: 'pid,hostname',
-      },
-    }
+    target: 'pino-pretty',
+    options: {
+      colorize: true,
+      translateTime: 'SYS:HH:MM:ss Z',
+      ignore: 'pid,hostname',
+    },
+  }
   : undefined;
 
 export const logger = pino({
